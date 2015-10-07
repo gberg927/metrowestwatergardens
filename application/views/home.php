5adaf51abec2ae8266b24b22bd178cb62034d325
Dennis Glasberg <dglasberg@asascience.com>
2015-10-07 16:17:37 -0400

    New layout

---
+++ application/views/home.php
@@ -0,0 +1,203 @@
+<html>
+<head>
+	<title>MetroWest Water Gardens</title>
+	<meta name="google-site-verification" content="onnkWmd6hPTa5aFMakZMmBpQatA8D0kGhVr0ueAED48" />
+	<link rel="shortcut icon" type="image/png" href="<? echo base_url(); ?>/images/favicon.png" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?echo base_url("includes/animate.min.css"); ?>">
+	<link rel="stylesheet" href="<?echo base_url("includes/carousel.css"); ?>">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="<?echo base_url("includes/carousel.js"); ?>"></script>
+	<style media="screen">
+		.theme-dropdown .dropdown-menu {
+			position: static;
+			display: block;
+			margin-bottom: 20px;
+		}
+
+		.theme-showcase > p > .btn {
+			margin: 5px 0;
+		}
+
+		.theme-showcase .navbar .container {
+			width: auto;
+		}
+
+		.navbar {
+			height: 135px;
+		}
+
+		.navbar-nav li a {
+			line-height:  135px;
+		    height: 135px;
+		    padding-top: 0;
+		}
+
+		.footer {
+			padding-top: 40px;
+			padding-bottom: 40px;
+			margin-top: 40px;
+			border-top: 1px solid #eee;
+		}
+
+		.carousel-control {
+		  top:50%;
+		  width:auto;
+		  height:1em;
+		  background:transparent;
+		}
+		.carousel-control .fa-arrow-circle-o-left, .carousel-control .fa-arrow-circle-o-right  {
+		  position:static;
+		  display:block;
+		  width:auto;
+		}
+		.carousel-control .fa-arrow-circle-o-left:before {
+		  margin-left:15px;
+		}
+
+		.carousel-control .fa-arrow-circle-o-right:before {
+		  margin-right:15px;
+		}
+	</style>
+</head>
+<body>
+
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<img alt="MetroWest Water Gardens" src="<?echo base_url("images/logo.png"); ?>" class="img-responsive">
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">About</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Photo Gallery</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container" role="main">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				</ol>
+
+				<div class="carousel-inner" role="listbox">
+					<div class="item active" style="background-image: url(<?echo base_url('images/slider/01.jpg'); ?>)";>
+					</div>
+					<div class="item" style="background-image: url(<?echo base_url('images/slider/02.jpg'); ?>)";>
+
+					</div>
+					<div class="item" style="background-image: url(<?echo base_url('images/slider/03.jpg'); ?>)";>
+
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    				<i class="fa fa-arrow-circle-o-left fa-3x"></i>
+    				<span class="sr-only">Previous</span>
+  				</a>
+  				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    				<i class="fa fa-arrow-circle-o-right fa-3x"></i>
+    				<span class="sr-only">Next</span>
+  				</a>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Design</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-3 col-sm-2">
+						<i class="fa fa-pencil-square-o fa-5x"></i>
+					</div>
+					<div class="col-lg-9 col-sm-10">
+						<p>We specialize in designing, installing and maintaining water gardens and water features, because we have the knowledge and background to do it right! </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Install</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-3 col-sm-2">
+						<i class="fa fa-truck fa-5x"></i>
+					</div>
+					<div class="col-lg-9 col-sm-10">
+						<p>We create all types of water gardens and water features to fit your budget, focusing primarily on installing Aquascape Designs' Complete Pond Solutions. </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Maintain</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-3 col-sm-2">
+						<i class="fa fa-tachometer fa-5x"></i>
+					</div>
+					<div class="col-lg-9 col-sm-10">
+						<p>If you want your pond to stay in tip-top shape all season long and do not always have the time to check on filters, trim back and fertilize plants, and care for the pond, then have us do it for you!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Aquascape Certified</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-1 col-sm-2">
+						<img alt="Aquascape Certified" src="<?echo base_url("images/aquascape.png"); ?>" class="img-responsive">
+					</div>
+					<div class="col-lg-11 col-sm-10">
+						<p>We are proud to be an Aquascape Designs Certified Aquascape Contractor (CAC) and a member of the Aquascape Designs Water Garden Excellence Program , designing and installing what we believe to be the absolute best water garden and water feature systems available anywhere.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer class="footer">
+			<div class="row">
+				<div class="col-lg-4">
+					<p><b>Phone: </b>(508) 916-1535</p>
+				</div>
+				<div class="col-lg-4">
+					<p><b>Address: </b>24 Moore Rd, Hopedale, MA 01747</p>
+				</div>
+				<div class="col-lg-4">
+					<p><b>Email: </b>pondguys@metrowestwatergardens.com</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-lg-offset-4">
+					<p>&copy; <?echo date("Y") ?> - MetroWest Water Gardens. All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body
+</html>
