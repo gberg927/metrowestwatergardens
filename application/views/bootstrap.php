26528fb0b24ef186af7a010571910f700289f09c
Dennis Glasberg <dennis.glasberg@gmail.com>
2016-06-27 21:15:58 -0400

    First round of changes

---
+++ application/views/bootstrap.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Business Frontpage - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../mwwg-bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="../mwwg-bootstrap/css/site.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                  <img src="../mwwg-bootstrap/logo.png" alt="Metrowest Water Gardens">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Photo Gallery</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="business-header">
+
+    </header>
+    <div class="container content">
+        <div class="row">
+            <div class="col-sm-4 info">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h2>Design</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-4 info-img">
+                  <span class="fa-stack fa-3x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-book fa-stack-1x "></i>
+                  </span>
+                </div>
+                <div class="col-sm-8">
+                  <p>We specialize in designing, installing and maintaining water gardens and water features, because we have the knowledge and background to do it right!</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4 info">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h2>Install</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-4 info-img">
+                  <span class="fa-stack fa-3x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-tasks fa-stack-1x "></i>
+                  </span>
+                </div>
+                <div class="col-sm-8">
+                  <p>We create all types of water gardens and water features to fit your budget, focusing primarily on installing Aquascape Designs' Complete Pond Solutions.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4 info">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h2>Maintain</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-4 info-img">
+                  <span class="fa-stack fa-3x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-wrench fa-stack-1x "></i>
+                  </span>
+                </div>
+                <div class="col-sm-8">
+                  <p>If you want your pond to stay in tip-top shape all season long and do not always have the time to check on filters, trim back and fertilize plants, and care for the pond, then have us do it for you!</p>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div>
+              <h4>Metrowest Water Gardens</h4>
+              <p>"Your full service water feature specialist"</p><br>
+              <p>&copy; <?php echo date("Y"); ?></p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div>
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-map-marker fa-inverse fa-stack-1x "></i>
+              </span>
+              <p><span>24 Moore Rd</span> Hopedale, MA 01747</p>
+            </div>
+            <div>
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-phone fa-inverse fa-stack-1x "></i>
+              </span>
+              <p>(508) 916-1535</p>
+            </div>
+            <div>
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-envelope fa-inverse fa-stack-1x "></i>
+              </span>
+              <p><a href="mailto:pondguys@metrowestwatergardens.com">pondguys@metrowestwatergardens.com</a></p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <h5>About the company</h5>
+            <p>Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="../mwwg-bootstrap/js/jquery.js"></script>
+    <script src="../mwwg-bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/c272f4dbd3.js"></script>
+</body>
+</html>
